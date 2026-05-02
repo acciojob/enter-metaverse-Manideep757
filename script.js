02811dd35283520a683d4e417d3185f74f721fc5
Manideep757 <gollapellymanideep185@gmail.com>
2026-05-02 14:02:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let status = document.getElementById("status");
+let button = document.getElementById("enterBtn");
+button.addEventListener("click",function(){
+	status.innerHTML = "<h1>Entered Metaverse</h1>"; 
+})
+
